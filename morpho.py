6fbb74d098ccbf8c0f762cbab0e84efe895fc68f
barthelemypicard <barthelemy.picard@supelec.fr>
2015-12-16 10:28:53 +0100

    Markers mask handling added.

--- morpho.py
+++ morpho.py
@@ -4,8 +4,20 @@ import cv2
 import numpy as np
 from collections import deque # deque permet d'avoir une structure de piles
 from scipy.signal import argrelextrema
-import scipy.ndimage as ndimage
-import scipy.ndimage.filters as filters
+import scipy.misc
+import pylab as pl
+
+
+#Ajoute du bruit à une image
+def addBlur(im, std = -1):
+    im_max = np.max(im)
+    im_min = np.min(im)
+    h, w = im.shape
+    if std == -1:
+        std = (im_max-im_min) / 4
+    result = im + std*(np.random.rand(h, w))
+    return result
+    
 
 
 # Permet d'obtenir les voisins de (x,y) en 4 ou 8 connexités
@@ -23,17 +35,26 @@ def voisins(x,y,nb_connex):
         
     return voisin
 
-
-def hqWatershed(img, nb_connex = 4, mask = None):
+#Calcule le watershed en utilisant des files d'attentes hierarchiques
+#Entrées :
+# - img : image à traiter
+# - nb_connex : nombre de connexités (4 ou 8)
+# - mask : masque des marqueurs (si rien n'est donné, on calcule les maxima locaux)
+# Sortie :
+# - labels : image labelisée (0 pour les fronitères)
+def hqWatershed(img, nb_connex = 8, mask = None):
 
     L = [] # liste qui contiendra les coordonnées des minima locaux
+    tmp_L = deque() #Liste d'initialisation des marqueurs
     Marque = np.zeros(img.shape) # matrice qui indique si les pixels sont traités
     labels = np.zeros(img.shape) # matrice contenant les labels, les pixels ayant un label valant 0 à la fin du programme sont des points frontière
 
     k = 50 # taille du carré considéré pour calculer les minima locaux
     p = 1 # compteur qui sert d'indicateur de label
     
+    # Initialisation des marqueurs
     if mask == None:
+        #Si aps de masque en entrée, on calcule les minima locaux
         for i in range(0, np.size(img, 0) - k, k):
             for j in range(0, np.size(img, 1) - k, k):
                 # minMaxLoc retourne les valeurs et les positions des min et max de l'image considérée
@@ -48,25 +69,41 @@ def hqWatershed(img, nb_connex = 4, mask = None):
                     labels[a,b] = p
                     p += 1
     else:
-        (x_mask, y_mask) = mask
-        len = x_mask.shape
-        for i in range(len[0]):
-            L.append((x_mask[i],y_mask[i]))
-            Marque[x_mask[i],y_mask[i]] = 1
-            labels[x_mask[i],y_mask[i]] = p
-            p += 1
-        
-
-    #cv2.namedWindow('labels', cv2.WINDOW_NORMAL)
-    #cv2.imshow('labels', labels)
-    #cv2.waitKey(0)
+        print "Coucou"
+        mask_max = mask.max()
+        for i in range(np.size(img, 0)):
+            for j in range(np.size(img, 1)):
+                current_label = -1
+                if mask[i,j] == mask_max and Marque[i,j] == 0:
+                    current_label = p
+                    p += 1
+                    L.append((i,j))
+                    Marque[i,j] = 1
+                    labels[i,j] = current_label
+                    all_voisins = voisins(i,j,8)
+                    for vois in all_voisins:
+                        X = vois[0]
+                        Y = vois[1]
+                        # On parcourt les voisins de même label de proche en proche
+                        if tmp_L.count((X,Y)) == 0 and X >= 0 and Y >= 0 and X < np.size(img,0) and Y < np.size(img,1):
+                            if Marque[X,Y] == 0 and mask[X,Y] == mask_max:
+                                tmp_L.append((X,Y))
+                # Tant que toute la zone connexe n'est pas labelisée, on continue
+                while tmp_L:
+                    x, y = tmp_L.popleft()
+                    L.append((x,y))
+                    Marque[x,y] = 1
+                    labels[x,y] = current_label
+                    all_voisins = voisins(x,y,nb_connex)
+                    for vois in all_voisins:
+                        X = vois[0]
+                        Y = vois[1]
+                        if tmp_L.count((X,Y)) == 0 and X >= 0 and Y >= 0 and X < np.size(img,0) and Y < np.size(img,1):
+                            if Marque[X,Y] == 0 and mask[X,Y] == mask_max:
+                                tmp_L.append((X,Y))
+    print "Markers initialized"
     
-    #L.append((60,80)); Marque[60,80] = 1; labels[60,80] = 1
-    #L.append((140,100)); Marque[140,100] = 1; labels[140,100] = 2
-
-
     fifos = [deque() for i in range(256)] # liste des piles correspondant aux intensités des pixels
-
 
     # dans la boucle suivante, on considère les 4 plus proches voisins du minimum local considéré
     for marqueur in L:
@@ -78,17 +115,11 @@ def hqWatershed(img, nb_connex = 4, mask = None):
             if ((X,Y) not in L) and X >= 0 and Y >= 0 and X < np.size(img,0) and Y < np.size(img,1):
                 if ((X,Y) not in fifos[img[X,Y]]):
                     fifos[img[X,Y]].append((X,Y))
-        #if ((x-1,y) not in L):
-        #    fifos[img[x-1,y]].append((x-1,y))
-        #if ((x,y-1) not in L):
-        #    fifos[img[x,y-1]].append((x,y-1))
-        #if (((x,y+1) not in L) and (y+1 < np.size(img,1))):
-        #    fifos[img[x,y+1]].append((x,y+1))
-        #if (((x+1,y) not in L) and (x+1 < np.size(img,0))):
-        #    fifos[img[x+1,y]].append((x+1,y))
         
-
+    h, w = Marque.shape
+    n_px = h*w
     while (np.array_equal(Marque, np.ones(img.shape)) == False):
+        # print "%d%% achieved." % (np.sum(Marque)*100/n_px)
         x, y = -1, -1 # coordonnées du premier point d'intensité minimale dans les piles
         n = 0
     
@@ -102,8 +133,6 @@ def hqWatershed(img, nb_connex = 4, mask = None):
                 print "All queues empty"
                 break
         
-        
-########################################################################################
         Marque[x,y] = 1
         
         # On cherche à savoir si le pixel courant est un point frontière
@@ -125,69 +154,24 @@ def hqWatershed(img, nb_connex = 4, mask = None):
                 if not Marque[xv,yv] and ((xv,yv) not in fifos[img[xv,yv]]):
                     fifos[img[xv,yv]].append((xv,yv))
         if labels[x,y] == -1:
+            # Cas particulier où le point est indéterminé. On lui donne alors 
+            # un label arbitraire
             labels[x,y] = 1000
-        #if labels[x,y] >= 0:
-        #    Marque[x,y] = 1
-        #    for vois in all_voisins:
-        #        if xv >= 0 and yv >= 0 and xv < np.size(img,0) and yv < np.size(img,1):
-        #            if not Marque[xv,yv] and ((xv,yv) not in fifos[img[xv,yv]]):
-        #                fifos[img[xv,yv]].append((xv,yv))
-        #else:
-        #    if ((x,y) not in fifos[img[xv,yv]]):
-        #        fifos[img[x,y]].append((x,y))
-    
-    #    if (((x+1) < np.size(img,0)) and ((y+1) < np.size(img,1))):
-    #        if (Marque[x-1,y] + Marque[x,y-1] + Marque[x,y+1] + Marque[x+1,y] < 4):
-    #            if (((x-1,y) not in fifos[img[x-1,y]]) and (Marque[x-1,y] == 0)):
-    #                fifos[img[x-1,y]].append((x-1,y))
-    #            if (((x,y-1) not in fifos[img[x,y-1]]) and (Marque[x,y-1] == 0)):
-    #                fifos[img[x,y-1]].append((x,y-1))
-    #            if (((x,y+1) not in fifos[img[x,y+1]]) and (Marque[x,y+1] == 0)):
-    #                fifos[img[x,y+1]].append((x,y+1))
-    #            if (((x+1,y) not in fifos[img[x+1,y]]) and (Marque[x+1,y] == 0)):
-    #                fifos[img[x+1,y]].append((x+1,y))
-    #                
-    #    elif ((x+1) < np.size(img,0)):
-    #        if (Marque[x-1,y] + Marque[x,y-1] +Marque[x+1,y] < 3):
-    #            if (((x-1,y) not in fifos[img[x-1,y]]) and (Marque[x-1,y] == 0)):
-    #                fifos[img[x-1,y]].append((x-1,y))
-    #            if (((x,y-1) not in fifos[img[x,y-1]]) and (Marque[x,y-1] == 0)):
-    #                fifos[img[x,y-1]].append((x,y-1))
-    #            if (((x+1,y) not in fifos[img[x+1,y]]) and (Marque[x+1,y] == 0)):
-    #                fifos[img[x+1,y]].append((x+1,y))
-    #                
-    #    elif ((y+1) < np.size(img,1)):
-    #        if (Marque[x-1,y] + Marque[x,y-1] +Marque[x,y+1] < 3):
-    #            if (((x-1,y) not in fifos[img[x-1,y]]) and (Marque[x-1,y] == 0)):
-    #                fifos[img[x-1,y]].append((x-1,y))
-    #            if (((x,y-1) not in fifos[img[x,y-1]]) and (Marque[x,y-1] == 0)):
-    #                fifos[img[x,y-1]].append((x,y-1))
-    #            if (((x,y+1) not in fifos[img[x,y+1]]) and (Marque[x,y+1] == 0)):
-    #                fifos[img[x,y+1]].append((x,y+1))
-    #                
-    #    else:
-    #        if 
-            
-    ########################################################################################
-            
-
-
-    #cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    #cv2.imshow('image',img)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
 
     return labels
 
+
+
 if __name__ == "__main__":
-    nb_connex = 4
+    nb_connex = 8
+    ext_mask = None
     img = cv2.imread("coins.png")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # conversion de l'image en niveaux de gris
     cv2.namedWindow('Base image', cv2.WINDOW_NORMAL)
     cv2.imshow('Base image',img)
     cv2.waitKey(1)
     
-    radius = 15
+    radius = 20
     kernel = np.ones((radius,radius),np.uint8)
     img_open = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
     cv2.namedWindow('Open image', cv2.WINDOW_NORMAL)
@@ -200,24 +184,36 @@ if __name__ == "__main__":
     Mint = np.zeros(img.shape)
     Mint[Mint_arg] = 1
     
-    Mext = hqWatershed(255-img_open, nb_connex);
-    Mext = (Mext == 0)
-    cv2.namedWindow('Mext image', cv2.WINDOW_NORMAL)
-    cv2.imshow('Mext image', Mext.astype(np.int64))
+    ext_mask = cv2.imread("coins_mask.png")
+    ext_mask = cv2.cvtColor(ext_mask, cv2.COLOR_BGR2GRAY)
+    ext_mask = scipy.misc.imresize(ext_mask, img.shape)
+    cv2.namedWindow('Mask image', cv2.WINDOW_NORMAL)
+    cv2.imshow('Mask image',ext_mask)
     cv2.waitKey(1)
     
-    msk = (Mint + Mext)/2;
-    msk = (msk > 0);
-    cv2.namedWindow('mask image', cv2.WINDOW_NORMAL)
-    cv2.imshow('mask image', msk.astype(np.int64))
-    cv2.waitKey(1)
-    
+    if ext_mask == None:
+        Mext = hqWatershed(255-img_open, nb_connex);
+        Mext = (Mext == 0)
+        cv2.namedWindow('Mext image', cv2.WINDOW_NORMAL)
+        cv2.imshow('Mext image', Mext.astype(np.int64))
+        cv2.waitKey(1)
+        
+        msk = (Mint + Mext)/2;
+        msk = (msk > 0);
+        cv2.namedWindow('mask image', cv2.WINDOW_NORMAL)
+        cv2.imshow('mask image', msk.astype(np.int64))
+        cv2.waitKey(1)
+    else:
+        msk = ext_mask
+        
     #Impose min
-    grad_modif = img_grad - msk
-    result = hqWatershed(grad_modif)
-    result = 255*(result - result.min()) / (result.max() - result.min())
+    # grad_modif = img_grad - msk
+    result = hqWatershed(img_grad, mask = msk)
+    result = 255*((result>0) - result.min()) / (result.max() - result.min())
     cv2.namedWindow('result image', cv2.WINDOW_NORMAL)
     cv2.imshow('result image', result)
-    cv2.waitKey(1)
     
-   # cv2.destroyAllWindows()
+    print "Done."
+    cv2.waitKey(0)
+    
+    cv2.destroyAllWindows()
